418e03ed9e688637804df0bb25f6ca5144d22efd
Mike Bombay <82775499+MikeBombay@users.noreply.github.com>
2021-08-01 18:04:49 -0400

    another test and push from root

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,5 +1,15 @@
 const Manager = require("../lib/Manager");
 
+test('creates a manager object', () => {
+    const man = new Manager("Manager Person", 2, "boss@manager.com", "665");
+    
+    expect(man.name).toBe("Manager Person");
+    expect(man.id).toBe(2);
+    expect(man.email).toBe("boss@manager.com");
+    expect(man.officeNo).toBe("665");
+});
+
+
 test("set officeNo", () => {
   const testValue = 665;
   const man = new Manager("Manager Person", 2, "boss@manager.com", testValue);
