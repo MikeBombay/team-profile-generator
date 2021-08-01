14d08d3f43011ddbd834d0a370a87fd1d2c53319
Mike Bombay <82775499+MikeBombay@users.noreply.github.com>
2021-08-01 17:05:57 -0400

    constructors

--- index.js
+++ index.js
@@ -29,5 +29,32 @@ function addEmployee() {
         message: "Enter team member's email address",
         name: "email"
     }])
+    .then(({employee, id, email, role}) => {
+        if (role === "Manager") {
+            return inquirer
+                .prompt([{
+                    type:'text',
+                    name: 'officeNo',
+                    message:"Enter Manager's office number"
+                },
+    
+               
+    // else if (role === "Engineer") {
+            return inquirer
+                .prompt([{
+                    type: 'text',
+                    name: 'github',
+                    message: "Enter Engineer's Github username"
+           
+     // else if (role === 'Intern') {
+             return inquirer
+                .prompt([{
+                    type:'text',
+                    name:'school',
+                    message: "What is the Intern's school?"
+                },
+                
+};
+
    
 }
