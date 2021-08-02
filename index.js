326a341588282325a29c0afb796fdde5402d5c6f
Mike Bombay <82775499+MikeBombay@users.noreply.github.com>
2021-08-01 20:40:42 -0400

    ostly working prompts

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const Employee = require('./lib/Employee');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
+const { generateKey } = require('crypto');
 const employees = [];
 
 function addEmployee() {
     inquirer.prompt([{
+        type: "text",
         message: "Enter team member's name",
         name: "name"
     },
@@ -29,32 +31,76 @@ function addEmployee() {
         message: "Enter team member's email address",
         name: "email"
     }])
-    .then(({employee, id, email, role}) => {
+    .then(({name, role, id, email}) => {
         if (role === "Manager") {
-            return inquirer
-                .prompt([{
-                    type:'text',
-                    name: 'officeNo',
-                    message:"Enter Manager's office number"
-                },
-    
-               
-    // else if (role === "Engineer") {
-            return inquirer
-                .prompt([{
-                    type: 'text',
-                    name: 'github',
-                    message: "Enter Engineer's Github username"
-           
-     // else if (role === 'Intern') {
-             return inquirer
-                .prompt([{
-                    type:'text',
-                    name:'school',
-                    message: "What is the Intern's school?"
-                },
-                
-};
+            return inquirer.prompt([{
+                type:'text',
+                name: 'officeNo',
+                message:"Enter Manager's office #"
+            },
+            {
+                type: "list",
+                message: "Would you like to add more team members?",
+                choices: [
+                "yes",
+                "no"
+                ],
+                name: "addMore"
+                }]).then(({officeNo, addMore})=>{
+                    employees.push(new Manager(name, role, id, email, officeNo))
+                        console.log(employees)
+                        if (addMore === "yes") {addEmployee();
+                        } else {
+                            generate()
+                        }
+                    
+        if (role === "Intern") {
+            return inquirer.prompt([{
+                type:'text',
+                name: 'school',
+                message:"Enter Intern's school"
+            },
+            {
+                type: "list",
+                message: "Would you like to add more team members?",
+                choices: [
+                "yes",
+                "exit"
+                ],
+                name: "addMore"
+            }])
+            .then(({school, addMore})=> {
+                employees.push(new Intern(name, role, id, email, school))
+                if (addMore === "yes") {
+                    addEmployee();
+                } else {
+                    generate()
+                }
+            
 
-   
-}
+        if (role === "Engineer") {
+            return inquirer.prompt([{
+                type:'text',
+                name: 'github',
+                message:"Enter Engineer's Gihub ID"
+                },
+                {
+                type: "list",
+                message: "Would you like to add more team members?",
+                choices: [
+                    "yes",
+                    "no"
+                    ],
+                    name: "addMore"
+                }])
+                .then(({github, addMore})=> {
+                    employees.push(new Engineer(name, role, id, email, github))
+                    if (addMore === "yes") {
+                        addEmployee();
+                        console.log(employees)
+                    } else {
+                        generate()
+                    }
+                })}})}})}})};
+
+                addEmployee()
